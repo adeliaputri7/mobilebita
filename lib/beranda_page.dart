47df041446fc3caacdb4cc782c9dff3e9af7c127
bayu000o <bayjogre@gmail.com>
2025-04-22 10:25:21 +0700

    testes

---
+++ lib/beranda_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class BerandaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Ini Halaman Beranda'));
+  }
+}
